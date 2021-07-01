b5b7788f0a25dbd380f79e5caf83e22c1e546758
tokuhausu <38522336+tokuhausu@users.noreply.github.com>
2021-07-01 17:43:50 +0900

    add

--- ParseTest/Program.cs
+++ ParseTest/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Net.Http;
+using System.Linq;
+using System.Collections.Generic;
+using AngleSharp.Dom;
 
 namespace ParseTest
 {
@@ -6,7 +12,17 @@ namespace ParseTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            String address = "https://docs.google.com/forms/d/e/1FAIpQLSdj2X4LWjJQW13kSOU6BUbNy7xiS3fllzYdx8nDfzY_PVYjFg/viewform";
+            HttpResponseMessage responce = client.GetAsync(address).Result;
+            var parser = new HtmlParser();
+            IHtmlDocument doc = parser.ParseDocument(responce.Content.ReadAsStringAsync().Result);
+            Console.WriteLine(doc.GetElementsByClassName("freebirdFormviewerViewNumberedItemContainer").Length);
+            List<IElement> list = doc.GetElementsByClassName("freebirdFormviewerViewNumberedItemContainer").SelectMany(x=>x.Children).Where(x=>x.HasAttribute("data-params")).ToList();
+            foreach(IElement e in list)
+            {
+                Console.WriteLine(e.GetAttribute("data-params"));
+            }
         }
     }
 }
